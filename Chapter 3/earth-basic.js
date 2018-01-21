441b60e2f186b6d155d82502b8d70f7a5fa217c3
winnie <caiwili@qq.com>
2018-01-21 15:05:13 +0800

    更新

--- Chapter 3/earth-basic.js
+++ Chapter 3/earth-basic.js
@@ -1,7 +1,7 @@
 // Constructor
 EarthApp = function()
 {
-	Sim.App.call(this);
+    Sim.App.call(this);
 }
 
 // Subclass Sim.App
@@ -10,11 +10,11 @@ EarthApp.prototype = new Sim.App();
 // Our custom initializer
 EarthApp.prototype.init = function(param)
 {
-	// Call superclass init code to set up scene, renderer, default camera
-	Sim.App.prototype.init.call(this, param);
-	
+    // Call superclass init code to set up scene, renderer, default camera
+    Sim.App.prototype.init.call(this, param);
+
     // Create the Earth and add it to our sim
-    var earth = new Earth();
+    earth = new Earth();
     earth.init();
     this.addObject(earth);
 }
@@ -22,7 +22,7 @@ EarthApp.prototype.init = function(param)
 // Custom Earth class
 Earth = function()
 {
-	Sim.Object.call(this);
+    Sim.Object.call(this);
 }
 
 Earth.prototype = new Sim.Object();
@@ -32,22 +32,24 @@ Earth.prototype.init = function()
     // Create our Earth with nice texture
     var earthmap = "../images/earth_surface_2048.jpg";
     var geometry = new THREE.SphereGeometry(1, 32, 32);
-    var texture = THREE.ImageUtils.loadTexture(earthmap);
+    // 加载纹理
+    var loader = new THREE.TextureLoader()
+    var texture = loader.load(earthmap);
     var material = new THREE.MeshBasicMaterial( { map: texture } );
-    var mesh = new THREE.Mesh( geometry, material ); 
+    var mesh = new THREE.Mesh( geometry, material );
 
     // Let's work in the tilt
     mesh.rotation.x = Earth.TILT;
-    
+
     // Tell the framework about our object
-    this.setObject3D(mesh);    
+    this.setObject3D(mesh);
 }
 
 Earth.prototype.update = function()
 {
-	// "I feel the Earth move..."
-	this.object3D.rotation.y += Earth.ROTATION_Y;
+    // "I feel the Earth move..."
+    this.object3D.rotation.y += Earth.ROTATION_Y;
 }
 
-Earth.ROTATION_Y = 0.0025;
-Earth.TILT = 0.41;
+Earth.ROTATION_Y = 0.0025; // 自转
+Earth.TILT = 0.41; // 倾斜
--- Chapter 3/earth-lit.js
+++ Chapter 3/earth-lit.js
@@ -1,7 +1,7 @@
 // Constructor
 EarthApp = function()
 {
-	Sim.App.call(this);
+    Sim.App.call(this);
 }
 
 // Subclass Sim.App
@@ -10,24 +10,23 @@ EarthApp.prototype = new Sim.App();
 // Our custom initializer
 EarthApp.prototype.init = function(param)
 {
-	// Call superclass init code to set up scene, renderer, default camera
-	Sim.App.prototype.init.call(this, param);
-	
+    // Call superclass init code to set up scene, renderer, default camera
+    Sim.App.prototype.init.call(this, param);
+
     // Create the Earth and add it to our sim
     var earth = new Earth();
     earth.init();
     this.addObject(earth);
-    
-    // Let there be light!
-    var sun = new Sun();
-    sun.init();
-    this.addObject(sun);
+
+    var sun = new Sun()
+    sun.init()
+    this.addObject(sun)
 }
 
 // Custom Earth class
 Earth = function()
 {
-	Sim.Object.call(this);
+    Sim.Object.call(this);
 }
 
 Earth.prototype = new Sim.Object();
@@ -37,40 +36,37 @@ Earth.prototype.init = function()
     // Create our Earth with nice texture
     var earthmap = "../images/earth_surface_2048.jpg";
     var geometry = new THREE.SphereGeometry(1, 32, 32);
-    var texture = THREE.ImageUtils.loadTexture(earthmap);
+    // 加载纹理
+    var loader = new THREE.TextureLoader()
+    var texture = loader.load(earthmap);
+    // var material = new THREE.MeshBasicMaterial( { map: texture } );
     var material = new THREE.MeshPhongMaterial( { map: texture } );
-    var mesh = new THREE.Mesh( geometry, material ); 
+    var mesh = new THREE.Mesh( geometry, material );
 
     // Let's work in the tilt
     mesh.rotation.x = Earth.TILT;
-    
+
     // Tell the framework about our object
-    this.setObject3D(mesh);    
+    this.setObject3D(mesh);
 }
 
 Earth.prototype.update = function()
 {
-	// "I feel the Earth move..."
-	this.object3D.rotation.y += Earth.ROTATION_Y;
+    // "I feel the Earth move..."
+    this.object3D.rotation.y += Earth.ROTATION_Y;
 }
 
 Earth.ROTATION_Y = 0.0025;
 Earth.TILT = 0.41;
 
-// Custom Sun class
-Sun = function()
-{
-	Sim.Object.call(this);
+Sun = function () {
+    Sim.Object.call(this)
 }
+Sun.prototype = new Sim.Object()
 
-Sun.prototype = new Sim.Object();
+Sun.prototype.init = function () {
+    var light  = new THREE.PointLight(0xffffff, 2, 100)
+    light.position.set(-10, 0, 20)
 
-Sun.prototype.init = function()
-{
-    // Create a point light to show off the earth - set the light out back and to left a bit
-	var light = new THREE.PointLight( 0xffffff, 2, 100);
-	light.position.set(-10, 0, 20);
-    
-    // Tell the framework about our object
-    this.setObject3D(light);    
+    this.setObject3D(light)
 }
